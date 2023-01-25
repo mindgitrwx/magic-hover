766f688198d77d6af2f9b761ee188ab71923c463
Jonghyeon yeo <hybridkernal@gmail.com>
2023-01-25 19:12:44 +0900

    Add User-Agent header

--- arxiv_abstract.py
+++ arxiv_abstract.py
@@ -4,7 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 url = sys.argv[1]
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 abstract = soup.find('meta', attrs={'name':'citation_abstract'})['content']
 print(abstract)
