7e5ab2c5acf3777a786b5a38e7d814a65e8a7bc4
Jonghyeon yeo <hybridkernal@gmail.com>
2023-01-31 22:59:35 +0900

    Mod surround with main

--- arxiv_abstract.py
+++ arxiv_abstract.py
@@ -3,9 +3,10 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-url = sys.argv[1]
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
-abstract = soup.find('meta', attrs={'name':'citation_abstract'})['content']
-print(abstract)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    abstract = soup.find('meta', attrs={'name':'citation_abstract'})['content']
+    print(abstract)
